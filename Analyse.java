84a28a475f0696583876fa6c46c42c3b2fea1520
besenlohntsich <bessim.hasani@gmail.com>
2025-06-24 13:25:35 +0200

    überführung UML

--- Analyse.java
+++ Analyse.java
@@ -1,5 +1,10 @@
 import utils.*;
+import java.util.Scanner;
 public class Analyse {
+
+    private List<Token> tokenliste;
+    private Scanner scanner;
+    private Parser parser;
     private String eingabe;
     //eingabe wird zur analyse gespeichert
 
--- Verwaltung.java
+++ Verwaltung.java
@@ -1,3 +1,10 @@
 import utils.*;
+import java.util.Scanner;
 public class Verwaltung {
+
+    public static void Verwaltung(){
+        new Scanner = new Scanner(System.in);
+        System.out.println("Bitte Text zum überprüfen eingeben.");
+        new Analyse(scanner.next());
+    }
 }
