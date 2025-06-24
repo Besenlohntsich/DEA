d640cc73bf27bb9b5b810a914913f63cce358536
besenlohntsich <bessim.hasani@gmail.com>
2025-06-24 13:13:00 +0200

    überführung UML

--- Token.java
+++ Token.java
@@ -1,3 +1,24 @@
 import utils.*;
 public class Token {
+    private String wert;
+    private String typ;
+
+    public Token(String pWert,String pTyp){
+
+    }
+
+    public void setWert(String wert) {
+        this.wert = wert;
+    }
+    public String getWert(){
+
+    }
+
+    public void setTyp(String typ) {
+        this.typ = typ;
+    }
+
+    public String getTyp(){
+
+    }
 }
