be4efff60da23a0f15d04fe9f2fc617338245b52
Besenlohntsich <bessim.hasani@gmail.com>
2025-07-01 20:34:38 +0200

    implementation scanner

--- Scanner.java
+++ Scanner.java
@@ -1,21 +1,71 @@
 import utils.*;
 public class Scanner {
-
+    private Token aktuellesToken;
     private boolean fehler;
     private String eingabe;
+    private List tokenliste;
 
-    public Scanner(String eingabe,  List<Token> Tokenlist){
 
+    public Scanner(String eingabe, List<Token> Tokenlist) {
+        aktuellesToken = Tokenlist.getContent();
+        this.tokenliste = Tokenlist;
+        this.eingabe = eingabe;
+        this.fehler = false;
     }
 
-    public void scanne(){
+    public void scanne() {
 
+        int i;
+        for (i = 0; i < eingabe.length(); i += 2)
+        {
+            // Prüfe, ob noch mindestens 2 Zeichen übrig sind
+            if (i + 2 <= eingabe.length()) {
+                String ZweiBuchstaben = eingabe.substring(i, i + 2);
+
+                if (ZweiBuchstaben.equals("la")|| ZweiBuchstaben.equals("le") || ZweiBuchstaben.equals("lu"))
+                {
+                    tokenliste.append(new Token("Wort", ZweiBuchstaben));
+                    //"WORT" der Typ des Tokens ist, der angibt, welche Art von Token es ist (hier ein gültiges Wort der
+                    // Grammatik, z.B. "la", "le", "lu").
+                    //ZweiBuchstaben der Wert des Tokens ist, also der tatsächliche Text aus der Eingabe, der diesem Token entspricht.
+
+
+                } else {
+                    fehler = true;
+                    ausgabe(fehler);
+                    return;
+                }
+
+            }else {
+                // Restliche Zeichen sind weniger als 2 -> Fehler
+                fehler = true;
+                ausgabe(fehler);
+                return;
+            }
+            }
+
+        if(!fehler)
+        {
+         tokenliste.append(new Token("Ende",""));
+
+        }
     }
+
+
+
     private void ausgabe(boolean pFehler){
-
+        if (pFehler) {
+            System.out.println("Ungültiges Wort in der Eingabe gefunden.");
+        } else {
+            System.out.println("Eingabe erfolgreich gescannt, keine Fehler gefunden.");
+        }
     }
 
-    public List<Token> getTokenliste(){
-
+     public List<Token> getTokenliste(){
+         if (!fehler){
+             return tokenliste;
+         }else{
+             return null;
+         }
     }
 }
