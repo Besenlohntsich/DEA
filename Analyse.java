405b8d6d3436d7f9b7c4699de4729be510c17ffc
besenlohntsich <bessim.hasani@gmail.com>
2025-07-02 10:43:59 +0200

    Analye und Paraser implementation

--- Analyse.java
+++ Analyse.java
@@ -6,11 +6,31 @@ public class Analyse {
     private Scanner scanner;
     private Parser parser;
     private String eingabe;
-    //eingabe wird zur analyse gespeichert
 
-    public Analyse(String eingabe)
-    {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        while (true){
+            System.out.println("Welches Wort möchtest du Analysieren?");
+            new Analyse(scanner.next());
+        }
 
 
     }
+
+    public Analyse(String eingabe) {
+        this.eingabe = eingabe;
+        tokenliste=new List<>();
+        scanner=new Scannerh(eingabe,tokenliste);
+        parsser=new Parser(tokenliste);
+        analysiere();
+
+    }
+    public void Analysieren()
+    {
+        scanne();
+        if(!tokenliste.isEmpty()) {
+            parase();
+        }
+    }
+
 }
--- Paraser.java
+++ Paraser.java
@@ -1,17 +1,35 @@
 import utils.*;
 public class Paraser {
 
+    private boolean fehler;
+    private Token aktuellesToken;
+    private List tokenliste;
+
     public Paraser(Tokenlist){
+        fehler = false;
 
     }
 
     public nextToken(Token){
-
+    int i = 0;
+    tokenliste.toFirst();
+    tokenliste.getContent();
+    tokenliste.next();
     }
 
     public boolean parase(){
+    if(tokenliste.hasAccess())
+    {
+        pruefeA();
+        return true;
+    }else {
 
+   fehler=true;
     }
+        return false;
+    }
+
+
     public boolean pruefeS(){
 
     }
